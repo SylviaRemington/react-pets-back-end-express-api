29b19469171b82b350a1a67ee367d1f4f098d0a9
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 18:04:01 -0700

    adding controlllers directory with pets.js to import pets model

---
+++ controllers/pets.js
@@ -0,0 +1,7 @@
+// controllers/pets.js
+const Pet = require('../models/pet.js');
+
+
+// We need the Pet model in many of our routes, so we import it here from models/pets.js. 
+// Similarly to our models directory, we’ll be using a controllers directory 
+// to help keep a clear separation of concerns. 
