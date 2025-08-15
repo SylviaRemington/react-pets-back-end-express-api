f94f26fbf734dd59af1fa3a2c6e4b7286cd4db56
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 17:48:18 -0700

    changing the format of the schema so that i can read it better

--- models/pet.js
+++ models/pet.js
@@ -7,15 +7,8 @@ const mongoose = require('mongoose');
 // models/pet.js
 
 const petSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
+  name: {type: String, required: true,},
+  age: {type: Number, min: 0, required: true,},
   breed: String,
 });
 
