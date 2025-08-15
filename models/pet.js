889afd71de9319d52b560767e05862bbf2c17731
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 17:46:07 -0700

    schema notes added & petSchema added

--- models/pet.js
+++ models/pet.js
@@ -1,4 +1,22 @@
 // models/pet.js
-// Creating the pet schema
+// Creating the pet schema by first importing mongoose
 
 const mongoose = require('mongoose');
+
+// Creating the actual model/schema
+// models/pet.js
+
+const petSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  breed: String,
+});
+
+
