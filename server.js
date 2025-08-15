165b388f7ff649631c8a3f96621d07d60fb5ae2a
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 18:16:48 -0700

    addtl notes regarding routes in server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Imports
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -5,17 +6,30 @@ const app = express();
 const mongoose = require('mongoose');
 const logger = require('morgan');
 
+// Import the controller file
+const petRouter = require('./controllers/pets.js');
+
+// MONGO connect to database
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+
 app.use(express.json());
 app.use(logger('dev'));
 
+
 // Routes go here
+// Add the petRouter to the `/pets` route
+// All requests that we send to our application, if we want it to be sent to our pet router, it needs to be prepended with /pets
+app.use('/pets', petRouter);
+
+
+
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
 });
+
