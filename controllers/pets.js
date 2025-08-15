c33537a91000b66d49d0d849f42e63136f187e4b
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 18:05:14 -0700

    addtl notes

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,7 +1,12 @@
 // controllers/pets.js
+
+// adding the import statement here
 const Pet = require('../models/pet.js');
 
 
+
+
+// Addtl Notes:
 // We need the Pet model in many of our routes, so we import it here from models/pets.js. 
 // Similarly to our models directory, we’ll be using a controllers directory 
 // to help keep a clear separation of concerns. 
