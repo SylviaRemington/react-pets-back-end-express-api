faacffbc098d40c6a41fde53a187582977d212b3
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 17:51:54 -0700

    adding notes to schema model for pets

--- models/pet.js
+++ models/pet.js
@@ -1,15 +1,14 @@
 // models/pet.js
 // Creating the pet schema by first importing mongoose
-
 const mongoose = require('mongoose');
 
-// Creating the actual model/schema
-// models/pet.js
-
+// Creating the actual model/ Defining the schema
 const petSchema = mongoose.Schema({
   name: {type: String, required: true,},
   age: {type: Number, min: 0, required: true,},
   breed: String,
 });
 
+// registering the model
+const Pet = mongoose.model('Pet', petSchema);
 
