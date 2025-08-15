46eb7dc89bac7879501b2a00f683b6aa0e6cce02
SylviaRemington <sylviaremington@gmail.com>
2025-08-14 18:11:36 -0700

    adding imports and exports to the controller pet.js

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,10 +1,21 @@
 // controllers/pets.js
 
-// adding the import statement here
+// adding imports below:
+const express = require('express');
+const router = express.Router();
+
+// adding the import statement here:
 const Pet = require('../models/pet.js');
 
 
 
+
+
+
+
+
+// Export the router at the bottom of the file
+module.exports = router;
 
 // Addtl Notes:
 // We need the Pet model in many of our routes, so we import it here from models/pets.js. 
